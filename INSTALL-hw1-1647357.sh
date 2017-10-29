dfb724f9ebae3e0b5071ddf5955bc701feb4c175
pietroborrello <pietro.borrello95@gmail.com>
2017-10-29 16:20:58 +0100

    bug on install on gcc

--- INSTALL-hw1-1647357.sh
+++ INSTALL-hw1-1647357.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get install libgcrypt20 libgcrypt20-dev
-gcc -o hw1 hw1-1647357.c `libgcrypt-config --cflags --libs` -O1 -lpthread
+gcc -o hw1 hw1-1647357.c `libgcrypt-config --cflags --libs` -O1 -lpthread -lgpg-error
